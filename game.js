357a902460aefc4cc8cadecc1bdd477e0dadd987
Sebastian Lech <s.lech@hypermedia.pl>
2018-01-07 14:04:21 +0100

    Step 6a; put winning conditions to array

--- game.js
+++ game.js
@@ -27,36 +27,19 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function checkWinner() {
-        var row1 = fields[0].className + fields[1].className + fields[2].className;
-        var row2 = fields[3].className + fields[4].className + fields[5].className;
-        var row3 = fields[6].className + fields[7].className + fields[8].className;
-
-        var col1 = fields[0].className + fields[3].className + fields[6].className;
-        var col2 = fields[1].className + fields[4].className + fields[7].className;
-        var col3 = fields[2].className + fields[5].className + fields[8].className;
-
-        var diag1 = fields[0].className + fields[4].className + fields[8].className;
-        var diag2 = fields[2].className + fields[4].className + fields[6].className;
-
-        var redWon = "redredred";
-        var blueWon = "blueblueblue";
-        if (row1 === redWon
-            || row2 === redWon
-            || row3 === redWon
-            || col1 === redWon
-            || col2 === redWon
-            || col3 === redWon
-            || diag1 === redWon
-            || diag2 === redWon) {
+        var winning = [
+            fields[0].className + fields[1].className + fields[2].className,
+            fields[3].className + fields[4].className + fields[5].className,
+            fields[6].className + fields[7].className + fields[8].className,
+            fields[0].className + fields[3].className + fields[6].className,
+            fields[1].className + fields[4].className + fields[7].className,
+            fields[2].className + fields[5].className + fields[8].className,
+            fields[0].className + fields[4].className + fields[8].className,
+            fields[2].className + fields[4].className + fields[6].className
+        ];
+        if (winning.includes("redredred")) {
             alert("playerA won!");
-        } else if (row1 === blueWon
-            || row2 === blueWon
-            || row3 === blueWon
-            || col1 === blueWon
-            || col2 === blueWon
-            || col3 === blueWon
-            || diag1 === blueWon
-            || diag2 === blueWon) {
+        } else if (winning.includes("blueblueblue")) {
             alert("playerB won!");
         } else if (emptyFields === 0) {
             alert("It is a tie");
